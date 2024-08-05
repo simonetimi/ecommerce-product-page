8d8e2028e78dc9993cbdf1940676c9e141cbbf3e
Simone <150595274+simonetimi@users.noreply.github.com>
2024-08-05 10:40:47 +0200

    add avatar and cart icon with popover

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,42 @@
 import { Component } from '@angular/core';
 
+import {
+  HlmAvatarImageDirective,
+  HlmAvatarComponent,
+  HlmAvatarFallbackDirective,
+} from '@spartan-ng/ui-avatar-helm';
+
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
+import { provideIcons } from '@ng-icons/core';
+import { lucideShoppingCart } from '@ng-icons/lucide';
+
+import {
+  BrnPopoverCloseDirective,
+  BrnPopoverComponent,
+  BrnPopoverContentDirective,
+  BrnPopoverTriggerDirective,
+} from '@spartan-ng/ui-popover-brain';
+import {
+  HlmPopoverCloseDirective,
+  HlmPopoverContentDirective,
+} from '@spartan-ng/ui-popover-helm';
+
 @Component({
-  selector: 'header[appHeader]',
   standalone: true,
-  imports: [],
+  selector: 'header[appHeader]',
+  imports: [
+    HlmAvatarComponent,
+    HlmAvatarImageDirective,
+    HlmAvatarFallbackDirective,
+    HlmIconComponent,
+    BrnPopoverCloseDirective,
+    BrnPopoverComponent,
+    BrnPopoverContentDirective,
+    BrnPopoverTriggerDirective,
+    HlmPopoverCloseDirective,
+    HlmPopoverContentDirective,
+  ],
+  providers: [provideIcons({ lucideShoppingCart })],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
