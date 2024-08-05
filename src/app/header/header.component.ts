23e9e6938c957a6dc88f152db72fcc1e62f86ee7
Simone <150595274+simonetimi@users.noreply.github.com>
2024-08-05 09:23:53 +0200

    refactor navbar to avoid code duplication

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  menuItems = [
+    { id: 0, title: 'Collections' },
+    { id: 1, title: 'Men' },
+    { id: 2, title: 'Women' },
+    { id: 3, title: 'About' },
+  ];
+}
